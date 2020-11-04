a1960b252d2a6c1cd9675dd64d7c4b6801c569aa
jayson van olffen <jayson.vanolffen@student.rocva.nl>
2020-11-04 11:25:04 +0100

    begin to let or not to let.

--- 05 - Javascript/taak07 - Intelligente DOM/website/script.js
+++ 05 - Javascript/taak07 - Intelligente DOM/website/script.js
@@ -6,4 +6,9 @@ console.log(window.document);
 // document.querySelector("#posterimg1").src = null;
 
 
-// document.querySelector("#poster1");
+var elem = document.querySelector('#poster1');
+elem.style.backgroundColor = '#e5e5e5';
+var elem = document.querySelector('#poster2');
+elem.style.backgroundColor = '#FF8C00';
+var elem = document.querySelector('#poster3');
+elem.style.backgroundColor = '#00FA9A';
