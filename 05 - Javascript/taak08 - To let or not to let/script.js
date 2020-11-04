cf48031172bf9df50df6bd4f802faf85a172ab66
jayson van olffen <jayson.vanolffen@student.rocva.nl>
2020-11-04 11:56:38 +0100

    frankrijk done

---
+++ 05 - Javascript/taak08 - To let or not to let/script.js
@@ -0,0 +1,7 @@
+codingIsCool = true;
+codingIsCool = false;
+console.log("codingIsCool");
+huidigJaartal = 2020;
+geboorteJaar = 2004;
+mijnLeeftijd = huidigJaartal - geboorteJaar;
+console.log( mijnLeeftijd)
