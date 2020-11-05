9410e3a517c61da105f2ec378f02c732849e7b93
jayson van olffen <jayson.vanolffen@student.rocva.nl>
2020-11-05 15:07:13 +0100

    eindopdracht klaar

--- opdracht/eindopdracht/opdracht2/script.js
+++ opdracht/eindopdracht/opdracht2/script.js
@@ -1,27 +1,62 @@
-
-//voornaam
+-voornaam
 var voornaam = prompt("wat is je voornaam?");
 var element = document.getElementById("id01");
 element.innerHTML = voornaam;
 
-//achternaam
+-achternaam
 var achternaam = prompt("wat is je achternaam?");
 var element = document.getElementById("id02");
 element.innerHTML = achternaam;
 
-//email
+-email
 var email = prompt("wat is je email?");
 var element = document.getElementById("id03");
 element.innerHTML = email;
 
-//woonplaats
+-woonplaats
 var woonplaats = prompt("wat is je woonplaats?");
 var element = document.getElementById("id04");
 element.innerHTML = woonplaats;
 
-//leeftijd
+
+-leeftijd
 var leeftijd = prompt("wat is je leeftijd?");
 if (leeftijd == 16){
-    document.body.style.backgroundColor = "green";
+    document.body.style.backgroundColor = "orangered";
 }
+if (leeftijd == 17){
+    document.body.style.backgroundColor = "aqua";
+}
+if (leeftijd == 18){
+    document.body.style.backgroundColor = "goldenrod";
+}
+if (leeftijd == 19){
+    document.body.style.backgroundColor = "limegreen";
+}
+if (leeftijd == 20){
+    document.body.style.backgroundColor = "beige";
+}
+if (leeftijd == null){
+    document.body.style.backgroundColor = "darkgrey";
+}
+var element = document.getElementById("id05");
+element.innerHTML = leeftijd;
 
+ 
+///console
+let bezoeker ={
+    voornaam: voornaam,
+    achternaam: achternaam,
+    email: email,
+    woonplaats: woonplaats,
+    leeftijd: leeftijd
+}
+console.log(bezoeker);
+ 
+let naam ={
+    voornaam, 
+    achternaam
+}
+var element = document.getElementById("id06");
+element.innerHTML = voornaam +" " + achternaam;
+console.log(naam);
